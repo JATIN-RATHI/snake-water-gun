2da43b52b7862d19f605f5df3afd52fbfa900d1d
Jatin Rathi <65804761+JATIN-RATHI@users.noreply.github.com>
2020-06-25 09:19:55 +0530

    Update snake water gun game ( choice &random).py

--- snake water gun game ( choice &random).py
+++ snake water gun game ( choice &random).py
@@ -1,8 +1,9 @@
 def computer():
     import random
-    i = ["SNAKE","WATER","GUN"]
+    i = ["SNAKE", "WATER", "GUN"]
     res = random.choice(i)
     return res
+
 
 def user():
     print("Choose one from the below:")
@@ -11,24 +12,29 @@ def user():
     print("G: GUN")
     a = input().capitalize()
     return a
-    
 
-#main menu of the game
 
-print("---------WELCOME TO THE GAME---------")
+# main menu of the game
+from pygame import mixer
+print("\n\n---------WELCOME TO THE GAME---------")
 print("<><><><><><><><><><><><><><><><><><><>")
-print("|        SNAKE     WATER   GUN       |")
+print("||       SNAKE     WATER   GUN      ||")
 print("<><><><><><><><><><><><><><><><><><><>")
-print("\nDESCRIPTION:: WHO WILL SCORE MORE OUT OF 10 WILL BE WINNER")
-print("\n\t YOU    VS     COMPUTER")
+print("\nDESCRIPTION:: In this game Snake > Water , Water > Gun and Gun > Snake. WHO WILL SCORE MORE OUT OF 10 WILL BE WINNER.")
+name = input("\nEnter Your Name : ").capitalize()
+print("\n\n\t", name,"    VS     Computer")
 print("\n")
 
-i = 0 #loop initialize
-c = 0 #computer score
-u = 0 #user score
-d = 0 #draw
+
+mixer.init()
+mixer.music.load('c:/Users/JATIN RATHI/Downloads/snappie.mp3')
+mixer.music.play(100)
+i = 0  # loop initialize
+c = 0  # computer score
+u = 0  # user score
+d = 0  # draw
 while i < 10:
-    print("\n\n\t",i+1,"th game")
+    print("\n\n\t", i + 1, "th game")
     you = user()
     comp = computer()
     if comp == "SNAKE" and you == "W":
@@ -49,21 +55,28 @@ while i < 10:
     elif you == "W" and comp == "GUN":
         u = u + 1
         print("YOU WON")
-    else: 
-        d = d+1
+    else:
+        d = d + 1
         print("DRAW")
-   
-    i = i+1
-    
+
+    i = i + 1
+mixer.music.stop()
+
 print("\tGAME ENDED")
-print(" Computer's Total score is",c)
-print(" Your Total score is",u)
-print(" Total no. of draw matches:",d)
+print(" Computer's Total score is", c)
+print(  name,"'s Total score is", u)
+print(" Total no. of draw matches:", d)
 if c > u:
-    print(" You lost!!")
+    print(  name," lost!!")
     print(" Winner is Computer")
 elif u > c:
-    print(" Congratulations !!! you won by", u-c ,"points")
+    print(" Congratulations !!! you won by", u - c, "points")
 else:
     print(" Game is Draw")
+
+print("********  Coded by :    JATIN RATHI    *************")
+import time
+
+time.sleep(10)
+
     
